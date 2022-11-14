3c318e3c7b76961a5072109961851adec530147d
Katherine Cangahuamin <katherine.cangahuamin@epn.edu.ec>
2022-11-14 11:24:02 -0500

    Commit agragacion de ramas

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Auto a=new Auto();
         System.out.println("Caracteristicas Vehiculo:\n color: "+a.getColor()+"\n modelo: "+a.getTipo()+"\n marca: "+ a.getMarca());
-        System.out.println();
+        System.out.println("Probando ");
+        Rama r=new Rama();
+        System.out.println("Caracteristicas Vehiculo:\n color: "+r.getColor()+"\n modelo: "+r.getTipo()+"\n marca: "+ r.getMarca());
+
     }
 
 }
